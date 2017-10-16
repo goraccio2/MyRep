8eb2a9c991b3a5bf5a5a95f82a6472b772bbbdbe
goraccio2 <sokol_qa@yahoo.ca>
2017-10-16 12:47:21 -0400

    InitialSetup

---
+++ ConsoleApplication1/ConsoleApplication1/FirefoxDriver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace TollsQA
+{
+    class FirstTC
+    {
+        static void Main(string[] args)
+        {
+
+            FirefoxDriverService service = FirefoxDriverService.CreateDefaultService(@"C:\Users\esokol\source");
+            service.FirefoxBinaryPath = @"C:\Program Files\Mozilla Firefox\firefox.exe";
+            IWebDriver driver = new FirefoxDriver(service);
+
+            //           driver.Navigate().GoToUrl("http://google.ca");
+            driver.Url = "http://www.demoqa.com";
+        }
+    }
+}
